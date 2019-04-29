41db65bf99eab501ce8c48cddfc097c59f2d7d85
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-04-29 12:53:08 +0300

    HW2 Made a method findUuid() to remove duplication.

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -17,58 +17,54 @@ public class ArrayStorage {
     }
 
     public void update(Resume r) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(r.getUuid())) {
-                storage[i] = r;
-                break;
-            }
-            if (i == size - 1) {
-                System.out.println("Резюме с uuid = " + r.getUuid() + " нет!");
-            }
-
+        int i = findUuid(r.getUuid());
+        if (i != size) {
+            storage[i] = r;
+        } else {
+            System.out.println("Резюме с uuid = " + r.getUuid() + " нет!");
         }
     }
 
     public void save(Resume r) {
-        int i;
+        int i = findUuid(r.getUuid());
         if (size == storage.length) {
             System.out.println("Массив резюме полон!");
+        } else if (i == size) {
+            storage[size] = r;
+            size++;
         } else {
-            for (i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(r.getUuid())) {
-                    System.out.println("Резюме с uuid = " + r.getUuid() + " уже есть!");
-                    break;
-                }
-            }
-            if (i == size) {
-                storage[size] = r;
-                size++;
-            }
+            System.out.println("Резюме с uuid = " + r.getUuid() + " уже есть!");
         }
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
+        int i = findUuid(uuid);
+        if (i != size) {
+            return storage[i];
         }
         System.out.println("Резюме с uuid = " + uuid + " нет!");
         return null;
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
+        int i = findUuid(uuid);
+        if (i != size) {
+            storage[i] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("Резюме с uuid = " + uuid + " нет!");
+        }
+    }
+
+    private int findUuid(String uuid) {
+        int i;
+        for (i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
                 break;
             }
-            if (i == size - 1) {
-                System.out.println("Резюме с uuid = " + uuid + " нет!");
-            }
         }
+        return i;
     }
 
     /**
