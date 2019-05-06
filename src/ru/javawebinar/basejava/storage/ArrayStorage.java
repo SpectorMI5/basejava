2923750a2a0a2eb5027806c08e501e5f7cc0a5ce
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-05-06 12:37:20 +0300

    HW03 Upd ArrayStorage

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -1,34 +1,11 @@
 package ru.javawebinar.basejava.storage;
 
-import ru.javawebinar.basejava.model.Resume;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    public void save(Resume r) {
-        if (size == STORAGE_LIMIT) {
-            System.out.println("Массив резюме полон!");
-        } else if (getIndex(r.getUuid()) > -1) {
-            System.out.println("Резюме с id = " + r.getUuid() + " уже есть!");
-        } else {
-            storage[size] = r;
-            size++;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Резюме с id = " + uuid + " нет!");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
@@ -36,5 +13,14 @@ public class ArrayStorage extends AbstractArrayStorage {
             }
         }
         return -1;
+    }
+
+    @Override
+    protected void saveResume() {
+    }
+
+    @Override
+    protected void deleteResume(int i) {
+        storage[i] = storage[size - 1];
     }
 }
