b261ccd3ccbcb9ae11afca643c9ed1c565866fc2
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-05-06 13:58:40 +0300

    HW03 Change save() & delete()

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -6,46 +6,23 @@ import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-    public void save(Resume r) {
-        int i = size;
-        Resume buf;
-        if (size == STORAGE_LIMIT) {
-            System.out.println("Массив резюме полон!");
-        } else if (getIndex(r.getUuid()) > -1) {
-            System.out.println("Резюме с id = " + r.getUuid() + " уже есть!");
-        } else {
-            storage[size] = r;
-            if (size > 0) {
-                while (storage[i].compareTo(storage[i - 1]) < 0) {
-                    buf = storage[i];
-                    storage[i] = storage[i - 1];
-                    storage[i - 1] = buf;
-                    i--;
-                    if (i == 0) {
-                        break;
-                    }
-                }
-            }
-            size++;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            System.out.println("Резюме с id = " + uuid + " нет!");
-        } else {
-            for (index++; index < size; index++) {
-                storage[index - 1] = storage[index];
-            }
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
+    @Override
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+
+    @Override
+    protected void saveResume(Resume r, int i) {
+        i = Math.abs(i + 1);
+        System.arraycopy(storage, i, storage, i + 1, size - i);
+        storage[i] = r;
+    }
+
+    @Override
+    protected void deleteResume(int i) {
+        i++;
+        System.arraycopy(storage, i, storage, i - 1, size - i);
     }
 }
