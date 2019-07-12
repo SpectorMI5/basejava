aaa913b50aac8f3f1ff01413e44d2f194ebf8445
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-07-12 20:04:32 +0300

    HW05 Implement ListStorageTest

---
+++ test/ru/javawebinar/basejava/storage/ListStorageTest.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Test;
+
+public class ListStorageTest extends AbstractArrayStorageTest {
+
+    public ListStorageTest() {
+        super(new ListStorage());
+    }
+
+    @Test
+    public void saveOverflow() {
+    }
+}
