5fea3b00ccf906919491faa00ea34e4c1bd984b6
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-10-04 14:43:18 +0300

    HW12 Replace .forEach & count() with .reduce in minValue(); wrote down if inside .filter in oddOrEven()

--- src/ru/javawebinar/basejava/Streams_HW12.java
+++ src/ru/javawebinar/basejava/Streams_HW12.java
@@ -7,8 +7,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Streams_HW12 {
-    private static int multiplier = 1;
-    private static int result;
 
     public static void main(String[] args) {
         int[] values = {1, 2, 3, 3, 2, 3};
@@ -22,31 +20,30 @@ public class Streams_HW12 {
     }
 
     private static int minValue(int[] values) {
+        final int[] i = {1};
         List<Integer> numerals = new ArrayList<>();
-        IntStream.of(values)
+        return IntStream.of(values)
                 .distinct()
                 .boxed()
                 .sorted(Collections.reverseOrder())
-                .forEach(Streams_HW12::count);
-        return result;
-    }
-
-    private static void count(int x) {
-        result = result + x * multiplier;
-        multiplier = multiplier * 10;
+                .reduce(0, (acc, x) -> {
+                    acc = acc + x * i[0];
+                    i[0] = i[0] * 10;
+                    return acc;
+                });
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         int sum = IntStream.of(integers.stream().mapToInt(i -> i).toArray())
                 .sum();
-        if (sum % 2 == 0) {
-            return integers.stream()
-                    .filter(x -> x % 2 == 1)
-                    .collect(Collectors.toList());
-        } else {
-            return integers.stream()
-                    .filter(x -> x % 2 == 0)
-                    .collect(Collectors.toList());
-        }
+        return integers.stream()
+                .filter(x -> {
+                    if (sum % 2 == 0) {
+                        return x % 2 == 1;
+                    } else {
+                        return x % 2 == 0;
+                    }
+                })
+                .collect(Collectors.toList());
     }
 }
