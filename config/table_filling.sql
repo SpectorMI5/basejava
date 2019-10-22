0d72231559b96ffce2ddf49067ee9e2d62a38274
Evgeniy Titov <ewgeniy.titov@yandex.ru>
2019-10-22 19:14:10 +0300

    Lesson14 JOIN & Transaction_Batch

--- config/table_filling.sql
+++ config/table_filling.sql
@@ -1,4 +1,4 @@
 INSERT INTO resume (uuid, full_name) VALUES
-  ('uuid1', 'Name3'),
-  ('uuid2', 'Name2'),
-  ('uuid3', 'Name1');
+  ('7298d0ae-d0cc-4545-be83-b186b223a17b', 'Name3'),
+  ('d0137eac-b63d-42d8-89d8-bbacb9826ad8', 'Name2'),
+  ('ffce9439-e4b8-438f-9a13-2e5e8340a2a0', 'Name1');
